7849d4e9f4c1a584e05d9f56d56345a18823db04
tonyrud <tonrudny@gmail.com>
2018-01-28 12:30:28 -0500

    add card component

--- src/stories/cards/Card.vue
+++ src/stories/cards/Card.vue
@@ -1,47 +1,96 @@
+<template>
+  <div class="card" :class="{rotate: hover}">
+    <div class="card__side card__side--front">
+        <div class="card__picture card__picture--1">
+            &nbsp;
+        </div>
+        <h4 class="card__heading">
+            <span class="card__heading-span card__heading-span-1">
+              <slot name="heading"></slot>
+            </span>
+        </h4>
+        <div class="card__details">
+            <ul>
+                <li>3 day tours</li>
+                <li>Up to 30 people</li>
+                <li>2 tour guides</li>
+                <li>Sleep in cozy hotels</li>
+                <li>Difficulty: easy</li>
+            </ul>
+        </div>
+    </div>
+    <div class="card__side card__side--back card__side--back-secondary">
+        <div class="card__cta">
+            <div class="card__cta__price-box">
+                <p class="card__cta__price-only">Only</p>
+                <p class="card__cta__price-value">$297</p>
+            </div>
+            <a class="btn btn--white">Book now!</a>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    hover: {
+      type: Boolean,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import './../../styles/utils/_module.scss';
+@import './../../styles/main.scss';
+
 .card {
-  // FUNCTIONALITY
+  // functionality of card
   perspective: 150rem;
   -moz-perspective: 150rem;
   position: relative;
-  height: 52rem;
+  height: 54rem;
+  max-width: 340px;
+  color: $color-text-light;
 
   &__side {
-    height: 52rem;
-    transition: all 0.8s ease;
+    color: $color-white;
+    font-size: 2rem;
+    height: 54rem;
+    transition: all 800ms cubic-bezier(0.61, -0.35, 0.26, 1.12) 0ms;
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     -webkit-backface-visibility: hidden;
     backface-visibility: hidden;
-    border-radius: 3px;
+    border-radius: 5px;
     overflow: hidden;
-    box-shadow: 0 1.5rem 4rem rgba($color-black, 0.15);
+    box-shadow: 0 1.5rem 4rem rgba($color-black, 0.2);
 
     &--front {
       background-color: $color-white;
     }
-
     &--back {
-      transform: rotateY(180deg);
+      transform: rotateY(-180deg);
 
-      &-1 {
+      &-secondary {
         background-image: linear-gradient(
           to right bottom,
           $color-secondary-light,
           $color-secondary-dark
         );
       }
-
-      &-2 {
+      &-primary {
         background-image: linear-gradient(
           to right bottom,
           $color-primary-light,
           $color-primary-dark
         );
       }
-
-      &-3 {
+      &-tertiary {
         background-image: linear-gradient(
           to right bottom,
           $color-tertiary-light,
@@ -50,25 +99,23 @@
       }
     }
   }
-
-  &:hover &__side--front {
-    transform: rotateY(-180deg);
+  &.rotate {
+    &:hover .card__side--front {
+      transform: rotateY(180deg);
+    }
+    &:hover .card__side--back {
+      transform: rotateY(0);
+    }
   }
-
-  &:hover &__side--back {
-    transform: rotateY(0);
-  }
-
-  // FRONT SIDE STYLING
+  // front side styling
   &__picture {
     background-size: cover;
     height: 23rem;
     background-blend-mode: screen;
     -webkit-clip-path: polygon(0 0, 100% 0, 100% 85%, 0 100%);
     clip-path: polygon(0 0, 100% 0, 100% 85%, 0 100%);
-    border-top-left-radius: 3px;
-    border-top-right-radius: 3px;
-
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
     &--1 {
       background-image: linear-gradient(
           to right bottom,
@@ -77,60 +124,38 @@
         ),
         url(#{$path--assets}/img/nat-5.jpg);
     }
-
-    &--2 {
-      background-image: linear-gradient(
-          to right bottom,
-          $color-primary-light,
-          $color-primary-dark
-        ),
-        url(#{$path--assets}/img/nat-6.jpg);
-    }
-
-    &--3 {
-      background-image: linear-gradient(
-          to right bottom,
-          $color-tertiary-light,
-          $color-tertiary-dark
-        ),
-        url(#{$path--assets}/img/nat-7.jpg);
-    }
   }
-
   &__heading {
     font-size: 2.8rem;
     font-weight: 300;
     text-transform: uppercase;
-    text-align: right;
     color: $color-white;
     position: absolute;
     top: 12rem;
     right: 2rem;
     width: 75%;
+    text-align: right;
+    z-index: 1;
   }
-
   &__heading-span {
     padding: 1rem 1.5rem;
     -webkit-box-decoration-break: clone;
     box-decoration-break: clone;
-
-    &--1 {
+    &-1 {
       background-image: linear-gradient(
         to right bottom,
         rgba($color-secondary-light, 0.85),
         rgba($color-secondary-dark, 0.85)
       );
     }
-
-    &--2 {
+    &-2 {
       background-image: linear-gradient(
         to right bottom,
         rgba($color-primary-light, 0.85),
         rgba($color-primary-dark, 0.85)
       );
     }
-
-    &--3 {
+    &-3 {
       background-image: linear-gradient(
         to right bottom,
         rgba($color-tertiary-light, 0.85),
@@ -138,28 +163,24 @@
       );
     }
   }
-
   &__details {
     padding: 3rem;
-
     ul {
       list-style: none;
       width: 80%;
       margin: 0 auto;
-
+      color: $color-text-light;
       li {
         text-align: center;
         font-size: 1.5rem;
         padding: 1rem;
-
         &:not(:last-child) {
           border-bottom: 1px solid $color-gray-light;
         }
       }
     }
   }
-
-  // FRONT SIDE STYLING
+  // back side styling
   &__cta {
     position: absolute;
     top: 50%;
@@ -167,66 +188,56 @@
     transform: translate(-50%, -50%);
     width: 90%;
     text-align: center;
+    &__price-box {
+      text-align: center;
+      margin-bottom: 8rem;
+      color: $color-white;
+    }
+    &__price-only {
+      font-size: 1.4rem;
+      text-transform: uppercase;
+    }
+    &__price-value {
+      font-size: 6rem;
+      font-weight: 100;
+    }
   }
-
-  &__price-box {
-    text-align: center;
-    color: $color-white;
-    margin-bottom: 8rem;
-  }
-
-  &__price-only {
-    font-size: 1.4rem;
-    text-transform: uppercase;
-  }
-
-  &__price-value {
-    font-size: 6rem;
-    font-weight: 100;
-  }
-
-  //@include respond(tab-port) {
+  // @include respond(tab-port)
   @media only screen and (max-width: 56.25em), only screen and (hover: none) {
     height: auto;
-    border-radius: 3px;
-    background-color: $color-white;
-    box-shadow: 0 1.5rem 4rem rgba($color-black, 0.15);
-
+    border-radius: 5px;
+    background: white;
+    box-shadow: 0 1.5rem 4rem rgba($color-black, 0.2);
     &__side {
       height: auto;
       position: relative;
       box-shadow: none;
-
       &--back {
         transform: rotateY(0);
-        clip-path: polygon(0 15%, 100% 0, 100% 100%, 0% 100%);
+        clip-path: polygon(0 15%, 100% 0, 100% 100%, 0 100%);
       }
     }
-
     &:hover &__side--front {
       transform: rotateY(0);
     }
-
     &__details {
       padding: 1rem 3rem;
     }
-
-    // FRONT SIDE STYLING
+    // back side styling
     &__cta {
       position: relative;
-      top: 0%;
+      top: 0;
       left: 0;
       transform: translate(0);
       width: 100%;
       padding: 7rem 4rem 4rem 4rem;
-    }
-
-    &__price-box {
-      margin-bottom: 3rem;
-    }
-
-    &__price-value {
-      font-size: 4rem;
+      &__price-box {
+        margin-bottom: 3rem;
+      }
+      &__price-value {
+        font-size: 4rem;
+      }
     }
   }
 }
+</style>
---
+++ src/stories/cards/cards.stories.js
@@ -0,0 +1,42 @@
+import { storiesOf } from '@storybook/vue'
+import Card from './Card.vue'
+
+import {
+  withKnobs,
+  text,
+  number,
+  boolean,
+  array,
+  select,
+  color,
+  date,
+  button,
+} from '@storybook/addon-knobs/vue'
+
+const inputWrapperStyles = {
+  width: '500px',
+  padding: '1em',
+}
+
+storiesOf('Cards', module)
+  .addDecorator(() => ({
+    // Decorated with `story` component
+    template: `<div :style="styles"><story/></div>`,
+    data() {
+      return {
+        styles: inputWrapperStyles,
+      }
+    },
+  }))
+  .addDecorator(withKnobs)
+  .add('Basic Card', () => {
+    const cardTitle = text('Card Title', 'The Sea Explorer')
+    const hover = boolean('Rotatable Card', false)
+    return {
+      components: { Card },
+      template: `
+      <card :hover="${hover}">
+        <span slot="heading">${cardTitle}</span>
+      </card>`,
+    }
+  })
